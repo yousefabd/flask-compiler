cb1d94ecf5ecb9766990b44665cef185029e5ec0
yousefabd <youussef3@gmail.com>
2025-12-10 17:36:09 +0300

    AST

--- src/html/AntlrToHTMLDocument.java
+++ src/html/AntlrToHTMLDocument.java
@@ -1,6 +1,26 @@
 package html;
 
+import antlr.html.HTMLParser;
 import antlr.html.HTMLParserBaseVisitor;
+import html.models.ElementNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AntlrToHTMLDocument extends HTMLParserBaseVisitor<HTMLDocument> {
+    public List<String> sematicErrors = new ArrayList<>();
+    public HTMLDocument document;
+
+    @Override
+    public HTMLDocument visitHtmlDocument(HTMLParser.HtmlDocumentContext ctx) {
+        if(ctx.element().size() > 1){
+            sematicErrors.add("HTML Document can only have one root element.");
+            return null;
+        } else {
+            AntlrToHTMLNode nodeVisitor = new AntlrToHTMLNode(sematicErrors);
+            ElementNode root = (ElementNode) nodeVisitor.visitElement(ctx.element(0));
+            document = new HTMLDocument(root);
+            return document;
+        }
+    }
 }
--- src/html/HTMLDocument.java
+++ src/html/HTMLDocument.java
@@ -1,4 +1,7 @@
 package html;
 
-public class HTMLDocument {
+import html.models.ElementNode;
+import html.models.Node;
+
+public record HTMLDocument(ElementNode root) implements Node {
 }
