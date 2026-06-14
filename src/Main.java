aadb6d65a66eb6697198a983b7797dbe531d1dac
yousefabd <youussef3@gmail.com>
2026-06-14 20:41:35 +0300

    include test file

--- src/Main.java
+++ src/Main.java
@@ -86,7 +86,7 @@ public class Main {
     public static void jinja() throws IOException 
     {
 
-        CharStream input = CharStreams.fromFileName("tests/templates/type_error.html");
+        CharStream input = CharStreams.fromFileName("tests/templates/error_test.html");
         HTMLLexer lexer = new HTMLLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         HTMLParser parser = new HTMLParser(tokens);
